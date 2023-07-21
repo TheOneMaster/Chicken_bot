d06eb321db430c9743d1ed2f6a08c85fa23cbd55
TheOneMaster <25909677+TheOneMaster@users.noreply.github.com>
2023-07-22 01:16:21 +0530

    get accounts from database and return multiple embeds

--- src/slashCommands/getAccount.ts
+++ src/slashCommands/getAccount.ts
@@ -1,4 +1,8 @@
 import { EmbedBuilder, SlashCommandBuilder } from "discord.js";
+import { AppDataSource } from "../data-source";
+import { Account } from "../entity/Account";
+import { getAccountRank, getPUUIDDetails } from "../riotAPI/accountDetails";
+import { Region } from "../riotAPI/types";
 import { SlashCommand } from "../types";
 
 const command: SlashCommand = {
@@ -19,18 +23,50 @@ const command: SlashCommand = {
         await interaction.deferReply();
 
         const { user } = interaction;
-        const region = interaction.options.getString("region", true);
+        const region = interaction.options.getString("region", true) as Region;
 
-        const embedMessage = new EmbedBuilder().setTitle(`Account Details ${region.toUpperCase()}`)
-            .setColor(0x0099FF)
-            .setAuthor({ name: "Chicken Bot" })
-            .setURL("https://github.com/TheOneMaster/Chicken_bot")
-            .addFields(
-                { name: "Accounts", value: "Lorem Ipsum" }
-            )
-            .setTimestamp();
+        const accounts = await AppDataSource
+            .getRepository(Account)
+            .findBy({ region: region });
 
-        await interaction.editReply({ embeds: [embedMessage] })
+        // console.log(accounts);
+
+        const embeds = await Promise.all(accounts.map(async account => {
+
+            if (account.puuid) {
+                const accountDetails = await getPUUIDDetails(account.puuid);
+                const rankDetails = await getAccountRank(region, account.puuid);
+
+                const name = accountDetails.name ?? rankDetails.name;
+                const tag = accountDetails.tag ?? rankDetails.tag;
+
+                const title = `${name}#${tag}`;
+
+                const playerCard = accountDetails.card?.large;
+                const rankIcon = rankDetails.images.large;
+
+                return new EmbedBuilder().setTitle(title)
+                    .setURL(`https://tracker.gg/valorant/profile/riot/${name}%23${tag}/overview`)
+                    .setColor(0x00ffff)
+                    .setThumbnail(playerCard ?? rankIcon ?? null)
+                    .addFields([
+                        { name: "Username", value: account.accountName, inline: true },
+                        { name: "Password", value: account.password, inline: true },
+                        { name: "Rank", value: rankDetails.currenttierpatched, inline: true }
+                    ])
+                    .setFooter({ iconURL: playerCard ? rankIcon : undefined, text: null! })
+            }
+
+            return new EmbedBuilder().setColor(0x00ffff)
+                .addFields([
+                    { name: "Username", value: account.accountName, inline: true },
+                    { name: "Password", value: account.password, inline: true },
+                ])
+        }));
+
+        await interaction.editReply({
+            embeds: embeds,
+        })
 
     }
 }
